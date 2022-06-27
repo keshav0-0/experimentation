f53d540d24791f533ce570522db3c1e0fb3d829e
Keshav Sharma <keshav.sharma@alpha-grep.com>
2022-06-27 06:09:09 +0000

    making changes

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ import os, subprocess
 
 
 def osysNp(cmd, suppress_stderr=False):
-  if suppress_stderr:
-    stderr_pipe = subprocess.PIPE
-  else:
-    stderr_pipe = None
-  process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=stderr_pipe, shell=True)
-  os.waitpid(process.pid, 0)
-  output = process.stdout.read().strip().decode('utf-8')
-  error = ''
-  if process.stderr is not None:
-    error = process.stderr.read().strip().decode('utf-8')
-  return output,error
+    if suppress_stderr:
+        stderr_pipe = subprocess.PIPE
+    else:
+        stderr_pipe = None
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=stderr_pipe, shell=True)
+    os.waitpid(process.pid, 0)
+    output = process.stdout.read().strip().decode('utf-8')
+    error = ''
+    if process.stderr is not None:
+        error = process.stderr.read().strip().decode('utf-8')
+    return output,error
+
+
+osysNp("git checkout master")
+
+
+testAdd(1,2)
+
+
+osysNp("git checkout test")
+
+
+testAdd(1,2)
